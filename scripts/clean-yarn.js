00167b0a0dc9f7d6186aee9e085ccb82325ff82f
Kevin Ross <kevin.ross@alienfast.com>
2023-07-24 17:06:30 -0500

    stub in project configurations

---
+++ scripts/clean-yarn.js
@@ -0,0 +1,19 @@
+import { $ } from 'execa'
+import { rimraf as r } from 'rimraf'
+
+// TODO: promote this as a script to @alienfast/ci once it is stable
+
+// eslint-disable-next-line no-console
+console.log('Cleaning yarn...')
+
+await $`yarn tsc -b --clean`
+
+// packages/*/dist cloud/*/dist .eslintcache ./packages/*/*.log *.log
+await Promise.all([
+  r('./{packages,cloud}/*/node_modules', { glob: true }),
+  r('yarn.lock'),
+  r('node_modules'),
+  r('.yarn/install-state.gz'),
+]).then(() => {
+  $`yarn cache clean`
+})
